4d03370db93c97d88609cd3c838e401f7ba30a05
alexandre <alexandre.jacquot@epitech.eu>
2025-05-27 16:50:28 +0200

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@ const nextConfig = {
       return [
           {
               source: '/api/pokemon',
-              destination: '<https://44k2n93ea9.execute-api.eu-west-2.amazonaws.com/Prod/pokemon>/Prod/pokemon',
+              destination: 'https://44k2n93ea9.execute-api.eu-west-2.amazonaws.com/Prod/pokemon>/Prod/pokemon',
           },
           {
               source: '/api/pokemon/:id',
-              destination: '<https://44k2n93ea9.execute-api.eu-west-2.amazonaws.com/Prod/pokemon>/Prod/pokemon/:id',
+              destination: 'https://44k2n93ea9.execute-api.eu-west-2.amazonaws.com/Prod/pokemon>/Prod/pokemon/:id',
           },
       ];
   }
